8e5a95c5d0591cd9c88c8f1a959576c347a58545
Jack Huang <jack.huang007@gmail.com>
2014-05-26 18:02:51 +0800

    formatted 2014-05-26

--- ThinkInJava/src/generics/CountedObject.java
+++ ThinkInJava/src/generics/CountedObject.java
@@ -1,8 +1,15 @@
 //: generics/CountedObject.java
+package generics;
 
 public class CountedObject {
-  private static long counter = 0;
-  private final long id = counter++;
-  public long id() { return id; }
-  public String toString() { return "CountedObject " + id;}
-} ///:~
+	private static long counter = 0;
+	private final long id = counter++;
+
+	public long id() {
+		return id;
+	}
+
+	public String toString() {
+		return "CountedObject " + id;
+	}
+} // /:~
