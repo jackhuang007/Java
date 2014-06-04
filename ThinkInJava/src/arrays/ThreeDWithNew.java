d645592c1f199849bac861d07e7a8ac3db166c9b
Jack Huang <jack.huang007@gmail.com>
2014-06-04 17:53:36 +0800

    Till 16.6.2

--- ThinkInJava/src/arrays/ThreeDWithNew.java
+++ ThinkInJava/src/arrays/ThreeDWithNew.java
@@ -1,12 +1,14 @@
 //: arrays/ThreeDWithNew.java
+package arrays;
+
 import java.util.*;
 
 public class ThreeDWithNew {
-  public static void main(String[] args) {
-    // 3-D array with fixed length:
-    int[][][] a = new int[2][2][4];
-    System.out.println(Arrays.deepToString(a));
-  }
-} /* Output:
-[[[0, 0, 0, 0], [0, 0, 0, 0]], [[0, 0, 0, 0], [0, 0, 0, 0]]]
-*///:~
+	public static void main(String[] args) {
+		// 3-D array with fixed length:
+		int[][][] a = new int[2][2][4];
+		System.out.println(Arrays.deepToString(a));
+	}
+} /*
+ * Output: [[[0, 0, 0, 0], [0, 0, 0, 0]], [[0, 0, 0, 0], [0, 0, 0, 0]]]
+ */// :~
